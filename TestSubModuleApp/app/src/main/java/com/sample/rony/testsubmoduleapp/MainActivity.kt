5327211e9d0daa8ae388515e75ad4bb3e6775e69
ronybrosh <ronybrosh@gmail.com>
2019-03-31 05:48:59 +0100

    Adding project.

--- TestSubModuleApp/app/src/main/java/com/sample/rony/testsubmoduleapp/MainActivity.kt
+++ TestSubModuleApp/app/src/main/java/com/sample/rony/testsubmoduleapp/MainActivity.kt
@@ -2,11 +2,14 @@ package com.sample.rony.testsubmoduleapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.appVersion).text = "App Version: ${BuildConfig.VERSION_NAME}"
     }
 }
