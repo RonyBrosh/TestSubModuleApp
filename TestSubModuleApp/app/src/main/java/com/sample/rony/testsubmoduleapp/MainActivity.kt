76cbd0ae214cd85ceb5ee0d08e21cb52f7b3caed
ronybrosh <ronybrosh@gmail.com>
2019-03-31 06:17:42 +0100

    Adding project.

--- TestSubModuleApp/app/src/main/java/com/sample/rony/testsubmoduleapp/MainActivity.kt
+++ TestSubModuleApp/app/src/main/java/com/sample/rony/testsubmoduleapp/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.sample.rony.testsubmoduleapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
+import com.sample.rony.testsubmodulelibrary.TestAPI
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<TextView>(R.id.appVersion).text = "App Version: ${BuildConfig.VERSION_NAME}"
-        findViewById<TextView>(R.id.libraryVersion).text = "App Version: ${BuildConfig.VERSION_NAME}"
+        findViewById<TextView>(R.id.libraryVersion).text = "Library Version: ${TestAPI.getVersion()}"
     }
 }
